b3ee24ba228fec6fe063d8f93c223eaa4e31c179
Naveen Dongre <naveen@inito.com>
2023-01-20 04:06:21 +0530

    a file uncommented

--- src/__tests__/utils/utils.test.js
+++ src/__tests__/utils/utils.test.js
@@ -1,48 +1,48 @@
-// /* eslint-disable max-len */
-// /* eslint-disable import/extensions */
-// /* eslint-disable global-require */
-// /* eslint-disable no-undef */
+/* eslint-disable max-len */
+/* eslint-disable import/extensions */
+/* eslint-disable global-require */
+/* eslint-disable no-undef */
 
-// const {
-//   createMapper, generateTransactionNumber, generateAccountNo, getLastDayOfMonthYear, calculateAge,
-// } = require('../../utils/utils.js');
+const {
+  createMapper, generateTransactionNumber, generateAccountNo, getLastDayOfMonthYear, calculateAge,
+} = require('../../utils/utils.js');
 
-// describe('Testing utils', () => {
-//   it('checking create mapper', async () => {
-//     const response = createMapper();
-//     expect(response[0]).toBe(1);
-//     expect(response[1]).toBe(3);
-//     expect(response[2]).toBe(8);
-//     expect(response[3]).toBe(19);
-//   });
+describe('Testing utils', () => {
+  it('checking create mapper', async () => {
+    const response = createMapper();
+    expect(response[0]).toBe(1);
+    expect(response[1]).toBe(3);
+    expect(response[2]).toBe(8);
+    expect(response[3]).toBe(19);
+  });
 
-//   it('checking create mapper', async () => {
-//     const response = createMapper();
-//     expect(response[0]).toBe(1);
-//     expect(response[1]).toBe(3);
-//     expect(response[2]).toBe(8);
-//     expect(response[3]).toBe(19);
-//   });
+  it('checking create mapper', async () => {
+    const response = createMapper();
+    expect(response[0]).toBe(1);
+    expect(response[1]).toBe(3);
+    expect(response[2]).toBe(8);
+    expect(response[3]).toBe(19);
+  });
 
-//   it('generating transaction number', async () => {
-//     const response = generateTransactionNumber();
-//     expect(response).toBeGreaterThan(1000000000000n);
-//   });
+  it('generating transaction number', async () => {
+    const response = generateTransactionNumber();
+    expect(response).toBeGreaterThan(1000000000000n);
+  });
 
-//   it('generating account number', async () => {
-//     const response = generateAccountNo(10);
-//     expect(response).toBeDefined();
-//   });
+  it('generating account number', async () => {
+    const response = generateAccountNo(10);
+    expect(response).toBeDefined();
+  });
 
-//   it('get last day of a month', async () => {
-//     const response1 = await getLastDayOfMonthYear(2023, 0);
-//     const response2 = await getLastDayOfMonthYear(2023, 1);
-//     expect(response1).toBe(31);
-//     expect(response2).toBe(28);
-//   });
+  it('get last day of a month', async () => {
+    const response1 = await getLastDayOfMonthYear(2023, 0);
+    const response2 = await getLastDayOfMonthYear(2023, 1);
+    expect(response1).toBe(31);
+    expect(response2).toBe(28);
+  });
 
-//   it('calculate age', async () => {
-//     const response = await calculateAge(new Date('2001-04-11'));
-//     expect(response).toBe(21);
-//   });
-// });
+  it('calculate age', async () => {
+    const response = await calculateAge(new Date('2001-04-11'));
+    expect(response).toBe(21);
+  });
+});
