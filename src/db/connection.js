a77e55563ef0b9646c0bd473da9c808cde274f96
Naveen Dongre <naveen@inito.com>
2023-01-29 17:22:46 +0530

    commit2

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,14 +1,14 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const databaseName = process.env.DATABASE_NAME;
-// const databaseName = process.env.TEST_DATABASE_NAME;
+// const databaseName = process.env.DATABASE_NAME;
+const databaseName = process.env.TEST_DATABASE_NAME;
 const dbpassword = process.env.DB_PASSWORD;
 const pool = new Pool({
   user: 'postgres',
   host: 'localhost',
   database: databaseName,
-  password: dbpassword,
+  password: 'naveen',
   dialect: 'postgres',
   port: 5432,
 });
